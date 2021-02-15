70ec40d766f18bf1919f2b3aebf699537247aa4f
madelinemcdonald <mmm01@uw.edu>
2021-02-15 15:37:58 -0800

    Madeline fixed typos

--- summary.R
+++ summary.R
@@ -3,10 +3,10 @@ library(tidyverse)
 
 # How many individuals responded to this survey?
 
-num_respondees <- tech_data %>%
+num_respondents <- tech_data %>%
   nrow()
 
-# Find the average age of the respondees.
+# Find the average age of the respondents.
 
 avg_age <- tech_data %>%
   mutate(age_no_outliers = if_else(Age > 118, 0, Age)) %>%
@@ -15,24 +15,24 @@ avg_age <- tech_data %>%
 
 # Find the number of participants from each state.
 
-state_respondees <- tech_data %>%
+state_respondents <- tech_data %>%
   group_by(state) %>%
   summarize(num_of_state = length(state)) %>%
   View()
 
-# Find the number of respondees from each company size.
+# Find the number of respondents from each company size.
 company_size <- tech_data %>%
   group_by(no_employees) %>%
   summarize(num_of_employees = length(no_employees)) %>%
   View()
 
-# How frequently did a respondants mental health interfere with their work?  
+# How frequently did a respondents mental health interfere with their work?  
 work_interfere <- tech_data %>%
   group_by(work_interfere) %>%
   summarize(num_of_responses = length(work_interfere)) %>%
   View()
 
-# How easy did respondants find taking medical leave for mental health to be?
+# How easy did respondents find taking medical leave for mental health to be?
 take_leave <- tech_data %>%
   group_by(leave) %>%
   summarize(num_of_responses = length(leave)) %>%
