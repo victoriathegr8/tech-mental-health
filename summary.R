330f90ebbc83e2bd2aa8b527227a2395e89e245e
madelinemcdonald <mmm01@uw.edu>
2021-02-15 13:41:00 -0800

    .rmd file

---
+++ summary.R
@@ -0,0 +1,39 @@
+tech_data <- read.csv("survey.csv")
+library(tidyverse)
+
+# How many individuals responded to this survey?
+
+num_respondees <- tech_data %>%
+  nrow()
+
+# Find the average age of the respondees.
+
+avg_age <- tech_data %>%
+  mutate(age_no_outliers = if_else(Age > 118, 0, Age)) %>%
+  filter(age_no_outliers == max(age_no_outliers, na.rm = T)) %>%
+  pull(age_no_outliers)
+
+# Find the number of participants from each state.
+
+state_respondees <- tech_data %>%
+  group_by(state) %>%
+  summarize(num_of_state = length(state)) %>%
+  View()
+
+# Find the number of respondees from each company size.
+company_size <- tech_data %>%
+  group_by(no_employees) %>%
+  summarize(num_of_employees = length(no_employees)) %>%
+  View()
+
+# How frequently did a respondants mental health interfere with their work?  
+work_interfere <- tech_data %>%
+  group_by(work_interfere) %>%
+  summarize(num_of_responses = length(work_interfere)) %>%
+  View()
+
+# How easy did respondants find taking medical leave for mental health to be?
+take_leave <- tech_data %>%
+  group_by(leave) %>%
+  summarize(num_of_responses = length(leave)) %>%
+  View()
