d47e780b16bb76bc284417c16cc0a63ce4368c28
madelinemcdonald <mmm01@uw.edu>
2021-02-17 22:35:21 -0800

    average updated

--- summary.R
+++ summary.R
@@ -9,31 +9,40 @@ num_respondents <- tech_data %>%
 # Find the average age of the respondents.
 
 avg_age <- tech_data %>%
-  mutate(age_no_outliers = if_else(Age > 118, 0, Age)) %>%
-  filter(age_no_outliers == max(age_no_outliers, na.rm = T)) %>%
-  pull(age_no_outliers)
+  filter(14 < Age & Age < 118) %>%
+  summarize(Age = mean(Age, na.rm = T)) %>%
+  pull(Age)
 
-# Find the number of participants from each state.
-
+# Find the number of participants from each state and the number of respondents
+# in the state with the most respondents.
 state_respondents <- tech_data %>%
   group_by(state) %>%
-  summarize(num_of_state = length(state)) %>%
-  View()
+  summarize(num_of_state = length(state))
+View()
 
-# Find the number of respondents from each company size.
+most_state_respondents <- tech_data %>%
+  group_by(state) %>%
+  summarize(num_of_state = length(state)) %>%
+  filter(state == "CA") %>%
+  pull(num_of_state)
+
+# Find the company size with the largest number of respondents.
 company_size <- tech_data %>%
   group_by(no_employees) %>%
   summarize(num_of_employees = length(no_employees)) %>%
-  View()
+  filter(num_of_employees == max(num_of_employees, na.rm = T)) %>%
+  pull(no_employees)
 
-# How frequently did a respondents mental health interfere with their work?  
+# How frequently did a respondents mental health interfere with their work?
 work_interfere <- tech_data %>%
   group_by(work_interfere) %>%
   summarize(num_of_responses = length(work_interfere)) %>%
-  View()
+  filter(work_interfere == "Sometimes") %>%
+  pull(num_of_responses)
 
 # How easy did respondents find taking medical leave for mental health to be?
 take_leave <- tech_data %>%
   group_by(leave) %>%
   summarize(num_of_responses = length(leave)) %>%
-  View()
+  filter(leave == "Very difficult") %>%
+  pull(num_of_responses)
