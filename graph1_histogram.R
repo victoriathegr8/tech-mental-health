02505c5dc4bcee6d54820c59d1c9854065db1250
victoriathegr8 <vbnguyen@uw.edu>
2021-02-18 08:31:37 -0800

    created faceted histogram

---
+++ graph1_histogram.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+mental_health <- read.csv("https://raw.githubusercontent.com/info201a-w21/project-victoriathegr8/main/data/mental_health_in_tech/survey.csv?token=ASMBIFLEYLWXVAMZZG7G6DTAG7G7U") %>%
+  filter(Age > 14 & Age < 118)
+
+
+age_vs_leave <- ggplot(data = mental_health) +
+  geom_histogram(mapping = aes(x = Age)) +
+  facet_wrap(~leave)
+age_vs_leave + labs(title = "Difficulty of Obtaining Medical Leave vs. Age")
